f1ed6dff21a31d24d8704e59c46ae53d0987c87b
Gee Wang <54838305+wqhanginge@users.noreply.github.com>
2024-10-30 19:54:59 +0800

    <Bugfix>
    - Fix: Unknow type name 'size_t' error with GCC compiler. Add <stddef.h>
      for 'size_t'.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ ext__minicrypto = Extension(
 
 setup(
     name='pgmmvdec',
-    version='0.1.1',
+    version='0.1.2',
     description='Pixel Game Maker MV Decrypter',
     author='blluv and Gee Wang',
     packages=find_packages(),
